a259798ebcbf028f07452c282bc0f51d33d5ec90
Davydova <suxar190981@gmail.com>
2026-04-15 00:29:24 +0300

    Lab12: update version to 2.0.0

--- Program.cs
+++ Program.cs
@@ -2,5 +2,5 @@
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/health", () => new { status = "ok", timestamp = DateTime.UtcNow });
-app.MapGet("/version", () => new { name = "IsLabApp", version = "1.0.1-lab11" });
+app.MapGet("/version", () => new { name = "IsLabApp", version = "2.0.0-lab12" });
 app.Run();
